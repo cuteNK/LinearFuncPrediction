35df435ce57800540faa294a66b5775610b8c462
NK <zzum0701@naver.com>
2022-11-16 09:02:01 +0900

    y=2x+1의 형태로 변경

--- script.js
+++ script.js
@@ -3,15 +3,16 @@
  * and cleaned of missing data.
  */
 async function getData() {
-    const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
-    const carsData = await carsDataResponse.json();
-    const cleaned = carsData.map(car => ({
-        mpg: car.Miles_per_Gallon,
-        horsepower: car.Horsepower,
-    }))
-        .filter(car => (car.mpg != null && car.horsepower != null));
-
-    return cleaned;
+    let rawData = [];
+    for(let x=0; x<=25; x++)
+    {
+        let obj = {
+            "x" : x,
+            "y" : 2 * x + 1
+        }
+        rawData.push(obj);
+    }
+    return rawData;
 }
 
 function createModel() {
@@ -42,8 +43,8 @@ function convertToTensor(data) {
         tf.util.shuffle(data);
 
         // Step 2. Convert data to Tensor
-        const inputs = data.map(d => d.horsepower)
-        const labels = data.map(d => d.mpg);
+        const inputs = data.map(d => d.x)
+        const labels = data.map(d => d.y);
 
         const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
         const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
@@ -78,7 +79,7 @@ async function trainModel(model, inputs, labels) {
     });
 
     const batchSize = 32;
-    const epochs = 400;
+    const epochs = 200;
 
     return await model.fit(inputs, labels, {
         batchSize,
@@ -93,61 +94,61 @@ async function trainModel(model, inputs, labels) {
 }
 
 function testModel(model, inputData, normalizationData) {
-  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
+    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
 
-  // Generate predictions for a uniform range of numbers between 0 and 1;
-  // We un-normalize the data by doing the inverse of the min-max scaling
-  // that we did earlier.
-  const [xs, preds] = tf.tidy(() => {
+    // Generate predictions for a uniform range of numbers between 0 and 1;
+    // We un-normalize the data by doing the inverse of the min-max scaling
+    // that we did earlier.
+    const [xs, preds] = tf.tidy(() => {
 
-    const xs = tf.linspace(0, 1, 100);
-    const preds = model.predict(xs.reshape([100, 1]));
+        const xs = tf.linspace(0, 1, 100);
+        const preds = model.predict(xs.reshape([100, 1]));
 
-    const unNormXs = xs
-      .mul(inputMax.sub(inputMin))
-      .add(inputMin);
+        const unNormXs = xs
+            .mul(inputMax.sub(inputMin))
+            .add(inputMin);
 
-    const unNormPreds = preds
-      .mul(labelMax.sub(labelMin))
-      .add(labelMin);
+        const unNormPreds = preds
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
 
-    // Un-normalize the data
-    return [unNormXs.dataSync(), unNormPreds.dataSync()];
-  });
+        // Un-normalize the data
+        return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
 
-  const predictedPoints = Array.from(xs).map((val, i) => {
-    return {x: val, y: preds[i]}
-  });
+    const predictedPoints = Array.from(xs).map((val, i) => {
+        return {x: val, y: preds[i]}
+    });
 
-  const originalPoints = inputData.map(d => ({
-    x: d.horsepower, y: d.mpg,
-  }));
+    const originalPoints = inputData.map(d => ({
+        x: d.x, y: d.y,
+    }));
 
-  tfvis.render.scatterplot(
-    {name: 'Model Predictions vs Original Data'},
-    {values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
-    {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
-      height: 300
-    }
-  );
+    tfvis.render.scatterplot(
+        {name: '예측데이터 확인하기'},
+        {values: [originalPoints, predictedPoints], series: ['y=2x+1', '예측']},
+        {
+            xLabel: 'x',
+            yLabel: 'y',
+            height: 300
+        }
+    );
 }
 
 async function run() {
     // Load and plot the original input data that we are going to train on.
     const data = await getData();
     const values = data.map(d => ({
-        x: d.horsepower,
-        y: d.mpg,
+        x: d.x,
+        y: d.y,
     }));
 
     tfvis.render.scatterplot(
-        {name: 'Horsepower v MPG'},
+        {name: 'x v yy'},
         {values},
         {
-            xLabel: 'Horsepower',
-            yLabel: 'MPG',
+            xLabel: 'x',
+            yLabel: 'y',
             height: 300
         }
     );
